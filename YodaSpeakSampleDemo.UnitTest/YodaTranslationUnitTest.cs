531c2822f381d42709e222324b9ccb310707ecc8
Roger <rogsmn@hotmail.com>
2017-09-10 14:20:36 +0530

    Added unit test and ui test projects to solution

---
+++ YodaSpeakSampleDemo.UnitTest/YodaTranslationUnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace YodaSpeakSampleDemo.UnitTest
+{
+    [TestClass]
+    public class YodaTranslationUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
